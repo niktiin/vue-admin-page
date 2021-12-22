33b5bb5e590f76a25749fe216bd0289956011584
Nikita Nikitin <95482043+niktiin@users.noreply.github.com>
2021-12-22 23:20:36 +0300

    feat(Vuex): Получаю данные из API
    Получаю данные из API, а именно: список товаров.

--- src/store/index.js
+++ src/store/index.js
@@ -19,11 +19,14 @@ export default new Vuex.Store({
     }
   },
   actions: {
-    requestAPIGetItem (context) {
-      const url = 'http://localhost:3000/api/index.php/api/items'
-
-      context.commit('setItems', ['item'])
-      console.log(context)
+    async requestAPIGetItem (context) {
+      const api = 'http://localhost:3000/api/index.php/api/items'
+      const responce = await axios.get(api)
+      // Проверяем ответ на полноту и ошибки
+      if (!responce || responce.status !== 200) {
+        return
+      }
+      context.commit('setItems', responce.data)
     }
   },
   modules: {
